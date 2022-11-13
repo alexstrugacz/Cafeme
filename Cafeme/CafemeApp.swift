f856c1b38e9c8a95a91c6ff3f0c9cd124b840f26
Alex Strugacz <alexstrugacz@icloud.com>
2022-11-13 15:35:21 -0600

    Initial Commit

---
+++ Cafeme/CafemeApp.swift
@@ -0,0 +1,17 @@
+//
+//  CafemeApp.swift
+//  Cafeme
+//
+//  Created by Alex Strugacz on 11/13/22.
+//
+
+import SwiftUI
+
+@main
+struct CafemeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
